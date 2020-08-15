2367fd07246d9602d648d663ba4d93c5c9ee2b9a
Prim69 <almusawihashim@gmail.com>
2020-08-14 22:16:00 -0400

    Add files via upload

---
+++ src/Prim/RandomDeathMessages/Main.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Prim\RandomDeathMessages;
+
+use pocketmine\{Player, Server};
+use pocketmine\event\{Entity, Listener};
+
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+use pocketmine\plugin\Pluginbase;
+
+use pocketmine\utils\Config;
+
+class Main extends Pluginbase implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+	}
+
+	public function onKill(PlayerDeathEvent $event){
+		$player = $event->getPlayer();
+		$cause = $player->getLastDamageCause();
+		$name = $player->getName();
+		$msgs = $this->getConfig()->get("ArrayMessage");
+		$variable = $this->getConfig()->get("DeathMessage");
+		$color = $this->getConfig()->get("Color");
+		if($cause instanceof EntityDamageByEntityEvent) {
+			$d = $cause->getDamager();
+			if ($d instanceof Player) {
+				$hp = round($d->getHealth(), 2);
+				$dname = $d->getName();
+	$variable = str_replace(["%killer%", "%victim%", "%randommsg%"], [$dname, $name, $msgs[array_rand($msgs)]], $variable);
+				if($this->getConfig()->get("Health") === true) {
+					$event->setDeathMessage($variable . " §7[§c$hp" . "§7]");
+					if(isset($color)){
+						$event->setDeathMessage($color . $variable . " §7[§c$hp" . "§7]");
+					}
+				} else {
+					if(isset($color)){
+						$event->setDeathMessage($color . $variable);
+					} else {
+						$event->setDeathMessage($variable);
+					}
+				}
+			}
+			}
+		}
+}
