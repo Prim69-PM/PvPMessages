b03c760bbd085bc7e34ab3d6047c7dc41aa003ae
Prim69 <almusawihashim@gmail.com>
2021-03-31 14:08:30 -0400

    just a bit more

--- src/Prim/RandomDeathMessages/Main.php
+++ src/Prim/RandomDeathMessages/Main.php
@@ -15,7 +15,7 @@ class Main extends Pluginbase implements Listener{
 		$this->saveResource("config.yml");
 	}
 
-	public function onKill(PlayerDeathEvent $event){
+	public function onDeath(PlayerDeathEvent $event){
 		$player = $event->getPlayer();
 		$cause = $player->getLastDamageCause();
 		$name = $player->getName();
@@ -26,23 +26,24 @@ class Main extends Pluginbase implements Listener{
 			$d = $cause->getDamager();
 			if ($d instanceof Player) {
 				if($this->getConfig()->get("HealOnKill")) $d->setHealth(20);
-				
+
 				$hp = round($d->getHealth(), 2);
 				$dname = $d->getName();
 				$variable = str_replace(["%killer%", "%victim%", "%randommsg%"], [$dname, $name, $msgs[array_rand($msgs)]], $variable);
-				
+
 				if($this->getConfig()->get("Health")) {
 					$event->setDeathMessage("$variable §7[§c$hp" . "§7]");
 					if(isset($color)) $event->setDeathMessage($color . "$variable §7[§c$hp" . "§7]");
-				} else {
-					if(isset($color)){
-						$event->setDeathMessage($color . $variable);
-					} else {
-						$event->setDeathMessage($variable);
-					}
+					return;
 				}
+				
+				if(isset($color)){
+					$event->setDeathMessage($color . $variable);
+					return;
+				}
+				$event->setDeathMessage($variable);
 			}
 		}
 	}
-	
+
 }
