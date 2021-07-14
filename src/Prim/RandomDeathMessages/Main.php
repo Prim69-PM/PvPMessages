2c8b0fb96bd541de90b4eef296366ed8d93fbe63
Hashim <13991048+Prim69@users.noreply.github.com>
2021-07-14 17:00:06 -0400

    Update Main.php

--- src/Prim/RandomDeathMessages/Main.php
+++ src/Prim/RandomDeathMessages/Main.php
@@ -9,36 +9,45 @@ use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\plugin\PluginBase;
 
 class Main extends Pluginbase implements Listener{
+	
+	public $messages = [];
+	public $deathMessage = '';
+	public $color = '';
+	public $healOnKill = false;
+	public $health = false;
 
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveResource("config.yml");
+		$config = $this->getConfig();
+		$this->messages = $config->get("ArrayMessage");
+		$this->deathMessage = $config->get("DeathMessage");
+		$this->color = $config->get("Color");
+		$this->healOnKill = $config->get("HealOnKill");
+		$this->health = $config->get("Health");
 	}
 
 	public function onDeath(PlayerDeathEvent $event){
 		$player = $event->getPlayer();
 		$cause = $player->getLastDamageCause();
 		$name = $player->getName();
-		$msgs = $this->getConfig()->get("ArrayMessage");
-		$variable = $this->getConfig()->get("DeathMessage");
-		$color = $this->getConfig()->get("Color");
 		if($cause instanceof EntityDamageByEntityEvent) {
 			$d = $cause->getDamager();
 			if ($d instanceof Player) {
-				if($this->getConfig()->get("HealOnKill")) $d->setHealth(20);
+				if($this->healOnKill) $d->setHealth(20);
 
 				$hp = round($d->getHealth(), 2);
 				$dname = $d->getName();
-				$variable = str_replace(["%killer%", "%victim%", "%randommsg%"], [$dname, $name, $msgs[array_rand($msgs)]], $variable);
+				$variable = str_replace(["%killer%", "%victim%", "%randommsg%"], [$dname, $name, $this->messages[array_rand($this->messages)]], $this->deathMessage);
 
-				if($this->getConfig()->get("Health")) {
+				if($this->health) {
 					$event->setDeathMessage("$variable §7[§c$hp" . "§7]");
-					if(isset($color)) $event->setDeathMessage($color . "$variable §7[§c$hp" . "§7]");
+					if(isset($color)) $event->setDeathMessage($this->color . "$variable §7[§c$hp" . "§7]");
 					return;
 				}
 				
 				if(isset($color)){
-					$event->setDeathMessage($color . $variable);
+					$event->setDeathMessage($this->color . $variable);
 					return;
 				}
 				$event->setDeathMessage($variable);
